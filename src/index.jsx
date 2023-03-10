5b0d3184d8b6595d6a0fde63f82c23865996e8dc
ninth hoom <ninthflooor@gmail.com>
2023-03-10 11:31:41 +0900

    Make Square componenet

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Square } from "./components/square";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Square />
+    {/* Board */}
+    {/* Game */}
+  </React.StrictMode>
+);
